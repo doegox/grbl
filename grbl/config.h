5a1f6b55226375e863366eb4d762a53ab3259e64
Philippe Teuwen <phil@teuwen.org>
2022-03-23 18:31:58 +0100

    1220x615

--- grbl/config.h
+++ grbl/config.h
@@ -697,8 +697,8 @@
 #define DEFAULT_X_ACCELERATION (150.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
 #define DEFAULT_Y_ACCELERATION (150.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
 #define DEFAULT_Z_ACCELERATION (150.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-#define DEFAULT_X_MAX_TRAVEL 1200.0 // mm NOTE: Must be a positive value.
-#define DEFAULT_Y_MAX_TRAVEL 600.0 // mm NOTE: Must be a positive value.
+#define DEFAULT_X_MAX_TRAVEL 1220.0 // mm NOTE: Must be a positive value.
+#define DEFAULT_Y_MAX_TRAVEL 615.0 // mm NOTE: Must be a positive value.
 #define DEFAULT_Z_MAX_TRAVEL 200.0 // mm NOTE: Must be a positive value.
 #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
 #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
